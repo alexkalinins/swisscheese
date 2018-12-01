b3c8b0bc7e008bfcf6d9ef4f8f8327a493e251fb
poof <jmal2424@gmail.com>
2018-12-01 10:32:42 -0500

    Add keyboard action interface and some autogenerated key actions

---
+++ src/engine/keyboard/Keyboard.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2018 Alex Kalinins
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package engine.keyboard;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ * Keyboard listener class for game's ui
+ * 
+ * @author Alex Kalinins
+ * @since 2018-12-1
+ * @since v0.2
+ * @version v0.1
+ */
+public class Keyboard implements KeyListener {
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
